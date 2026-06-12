235497db255c9292bdd4ed329129f7e24c53755a
SaynaMukherjee <mukherjeesayna@gmail.com>
2026-06-12 10:48:37 +0530

    Styled navbar

--- Frontend/src/components/navbar.jsx
+++ Frontend/src/components/navbar.jsx
@@ -1,5 +1,19 @@
 function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav>
+      <h2>Rondhone Bangali</h2>
+
+     <ul>
+  <li><a href="#">Recipes</a></li>
+  <li><a href="#">Travel Diaries</a></li>
+  <li><a href="#">Writing Corner</a></li>
+  <li><a href="#">About Me</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+
+      <button>Search</button>
+    </nav>
+  );
 }
 
 export default Navbar;
